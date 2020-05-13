15ac2ef9659e75c65a31a76b842b1cf27aabc77e
rs.dev <remote-software-dev>
2020-05-13 11:25:19 +0700

    added dummy table with dummy data in the plugin

--- wp-content/plugins/apicall/includes/apicall-class.php
+++ wp-content/plugins/apicall/includes/apicall-class.php
@@ -33,54 +33,77 @@ class Apicall_Widget extends WP_Widget
         }
 
         //Widget content Output
-        echo esc_html__( 'Hello, This is plugin for api call!', 'apicall_domain' );
+        $myTable = '
+        <table style="width:100%">
+            <tr>
+                <th>Nr</th>
+                <th>Name</th>
+                <th>Username</th>
+                <th>Email</th>
+                <th>See Detail</th>
+                </tr>
+            <tr>
+                <td>1</td>
+                <td>Jill</td>
+                <td>Smith</td>
+                <td>jill.smith@gmail.de</td>
+                <td><a href="http://www.google.com">Detail</a>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Eve</td>
+                <td>Jackson</td>
+                <td>superjustice@gmail.de</td>
+                <td><a href="http://www.google.com">Detail</a>
+            </tr>
+      </table> 
+        ';
+        echo $myTable;
         
+
 
         echo $args['after_widget']; //Whatever you want to display after widget (<div>, etc)
     }
 
     /**
-	 * Back-end widget form.
-	 *
-	 * @see WP_Widget::form()
-	 *
-	 * @param array $instance Previously saved values from database.
-	 */
-	public function form( $instance ) {
-        $title = ! empty( $instance['title'] ) ? 
-        $instance['title'] : esc_html__( 'Plugin development', 'apicall_domain' );
-        
-        ?>
-		<p>
-		    <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>">
-                <?php esc_attr_e( 'Title:', 'apicall_domain' ); ?>
-            </label> 
-		    <input 
-                class="widefat" 
-                id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" 
-                name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" 
-                type="text" 
-                value="<?php echo esc_attr( $title ); ?>">
-		</p>
-        <?php 
+     * Back-end widget form.
+     *
+     * @see WP_Widget::form()
+     *
+     * @param array $instance Previously saved values from database.
+     */
+    public function form($instance)
+    {
+        $title = !empty($instance['title']) ?
+            $instance['title'] : esc_html__('Plugin development', 'apicall_domain');
+
+?>
+        <p>
+            <label for="<?php echo esc_attr($this->get_field_id('title')); ?>">
+                <?php esc_attr_e('Title:', 'apicall_domain'); ?>
+            </label>
+            <input class="widefat" id="<?php echo esc_attr($this->get_field_id('title')); ?>" name="<?php echo esc_attr($this->get_field_name('title')); ?>" type="text" value="<?php echo esc_attr($title); ?>">
+        </p>
+<?php
 
     }
 
     /**
-	 * Sanitize widget form values as they are saved.
-	 *
-	 * @see WP_Widget::update()
-	 *
-	 * @param array $new_instance Values just sent to be saved.
-	 * @param array $old_instance Previously saved values from database.
-	 *
-	 * @return array Updated safe values to be saved.
-	 */
-	public function update( $new_instance, $old_instance ) {
+     * Sanitize widget form values as they are saved.
+     *
+     * @see WP_Widget::update()
+     *
+     * @param array $new_instance Values just sent to be saved.
+     * @param array $old_instance Previously saved values from database.
+     *
+     * @return array Updated safe values to be saved.
+     */
+    public function update($new_instance, $old_instance)
+    {
         $instance = array();
-		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? 
-        sanitize_text_field( $new_instance['title'] ) : '';
-        
+        $instance['title'] = (!empty($new_instance['title'])) ?
+            sanitize_text_field($new_instance['title']) : '';
+
         return $instance;
     }
 }
